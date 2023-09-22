f311daad9f5b86878866f75c52092c4ed10df6f2
lilo sourire <lilo-jolie@hotmail.fr>
2023-09-22 19:39:27 +0100

    avancement pages -6-et formulaire

--- wp-content/themes/planty/header.php
+++ wp-content/themes/planty/header.php
@@ -20,6 +20,7 @@
 	<meta charset="<?php bloginfo('charset'); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<?php wp_head(); ?>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body <?php body_class(); ?>>
